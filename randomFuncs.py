849806c10afbcaaab37bbc11b122aadf8779c31c
wenjones <wenjones@cisco.com>
2017-05-02 06:43:21 -0700

    updated randomFuncs, add two new files

--- randomFuncs.py
+++ randomFuncs.py
@@ -4,7 +4,11 @@ import random
 
 
 x = random.random()
-y = random.randint(1,100)
-print x , y
-print int(x)
+y = random.randint(0,100-1)
+print 'x, y:',x , y
+print 'int(x):', int(x)
 
+go = 0
+while go < 10:
+    print 'random[index]:',random.randint(1,100)
+    go += 1
---
+++ testloop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#
+clear
+# python -W ignore longevity.py -h 25.52.101.95 -t record -c 10
+
+for i in {1..5}
+do
+   echo "Welcome $i times"
+   sleep 5
+done
